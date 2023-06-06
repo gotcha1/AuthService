7b8a156153d133a86ea001f6cc53d38a7e9dc339
gotcha1 <51114133+gotcha1@users.noreply.github.com>
2023-06-06 17:30:03 +0800

    Update description

--- src/app/models/user_model.py
+++ src/app/models/user_model.py
@@ -16,8 +16,9 @@ class User(Document):
 
 
 class UserRequest(BaseModel):
-    username: str
-    password: str
+    username: str = Field(..., description='Desired username for the account, with a minimum length of 3 characters and a maximum length of 32 characters')
+    password: str = Field(..., description='''Password for the account, with a minimum length of 8 characters and a maximum length of 32 characters
+    containing at least 1 uppercase letter, 1 lowercase letter, and 1 number.''')
 
     class Config:
         schema_extra = {
@@ -30,8 +31,8 @@ class UserRequest(BaseModel):
 
 
 class UserResponse(BaseModel):
-    success: bool = Field(..., description='Indicates the result of the account creation process')
-    reason: Optional[str] = Field(..., description='Specifies the reason for a failed account creation process')
+    success: bool = Field(..., description='Indicates the result of the account creation process.')
+    reason: Optional[str] = Field(..., description='Specifies the reason for a failed account creation process.')
 
     class Config:
         schema_extra = {
